19c92d61e707ac3bea49e0bf6ee3d359ea0b498c
Bharanitharan.Ragu <bharani3ran@gmail.com>
2024-01-06 23:25:26 -0500

    registry - grpc and rest service - grpc and rest clients all put together

--- registry/src/main/java/com/batty/registry/RestService.java
+++ registry/src/main/java/com/batty/registry/RestService.java
@@ -3,6 +3,7 @@ package com.batty.registry;
 import com.batty.registry.api.RegistryApi;
 import com.batty.registry.datastore.DatastoreImpl;
 import com.batty.registry.model.Error;
+import com.batty.registry.model.MongoDate;
 import com.batty.registry.model.ServiceSchema;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
@@ -10,13 +11,17 @@ import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Indexed;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import com.batty.registry.HelloRequest;
-import com.batty.registry.HelloResponse;
 import com.batty.registry.RestServiceGrpc.*;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 @Component("RegistryController")
@@ -44,6 +49,7 @@ public class RestService extends RestServiceImplBase implements RegistryApi  {
     @Override
     public ResponseEntity<ServiceSchema> getService(String serviceId) {
       try {
+
         return ResponseEntity.ok(datastore.findServiceById(serviceId));
       }
       catch(Exception e)
@@ -52,10 +58,20 @@ public class RestService extends RestServiceImplBase implements RegistryApi  {
       }
     }
 
+
+
   @Override
-  public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
-    HelloResponse reply = HelloResponse.newBuilder()
-            .setResp("Hello ==> " + request.getFirstName())
+  public void registerService(registerServiceRequest request, StreamObserver<registerServiceResponse> responseObserver) {
+      ServiceSchema schema = new ServiceSchema();
+      schema.setStatus(Boolean.toString(request.getStatus()));
+      schema.setServiceId(request.getServiceName());
+      schema.setServiceName(request.getServiceName());
+      schema.setServiceHostIP(request.getIp());
+
+
+      schema = datastore.addService(schema);
+      registerServiceResponse reply = registerServiceResponse.newBuilder()
+            .setServiceName(schema.getServiceName()).setIp(schema.getServiceHostIP()).setStatus(Boolean.getBoolean(schema.getStatus()))
             .build();
     responseObserver.onNext(reply);
     responseObserver.onCompleted();
